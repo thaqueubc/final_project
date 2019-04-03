41b95d26291d37fbab1dd89782053a8af2c71154
Tasnuva Haque <reya.ruet@gmail.com>
2019-04-02 21:05:06 -0700

    final updates

--- scripts/showHideDiv.js
+++ scripts/showHideDiv.js
@@ -1,9 +1,23 @@
 // JavaScript Document
 
+// Grab all the outline section links
+const $outlineSectionLinks	= $('.outline-section-link');
+// Grab all the outline sections
+const $outlineSections = $('.outline-section');
+
+$outlineSections.not(':first').hide();
+
+$outlineSectionLinks.click(function(e){
+	e.preventDefault();
+	$outlineSections.hide();
+	$( $(this).data('id') ).show();
+});
+
+
 //Code courtesy http://jsfiddle.net/VpkKn/
 
-  $('a').on('click', function(){
-	var target = $(this).attr('rel');
-	$("#"+target).show().siblings("div").hide();
- });
+//   $('a').on('click', function(){
+// 	var target = $(this).attr('rel');
+// 	$("#"+target).show().siblings("div").hide();
+//  });
 
